62fc33408c95416d97211419b1bbf7f4ec02ac8c
Benny623 <bmilner88@gmail.com>
2022-05-18 15:01:48 -0600

    got it working

--- script.js
+++ script.js
@@ -1,44 +1,43 @@
-const groupId = document.querySelector('#group-id');
-const customerNum = document.querySelector('#customer-num');
 const createButton = document.querySelector('#create-btn');
 
 function downloadHandler(event) {
     event.preventDefault();
 
+    const groupId = document.querySelector('#group-id');
+    const customers = document.querySelector('#customer-num').value.split('\n');
+
+    if(!groupId.value || !customers.length) {
+        alert('Please fill out form')
+        return;
+    };
+
+    
+    const temp = [];
+    customers.forEach(customer => temp.push(`   <group-assignment group-id="${groupId.value}" customer-no="${customer}" />\n`));
+
+    console.log(temp)
+
     const filename = `${groupId.value}.xml`;
-    const xmltext = `
-<?xml version="1.0" encoding="UTF-8"?>
+    const xmltext = `<?xml version="1.0" encoding="UTF-8"?>
 <customer-groups xmlns="http://www.demandware.com/xml/impex/customergroup/2007-06-30">
 <customer-group group-id="${groupId.value}">
 <custom-attributes/>
 </customer-group>
-    <group-assignment group-id="${groupId.value}" customer-no="${customerNum.value}" />
+${temp.join('')}
 </customer-groups>   
 `
-    const download = document.createElement('a');
-    const bb = new Blob([xmltext], {type: 'text/plain'});
+    let download = document.createElement('a');
+    let bb = new Blob([xmltext], {type: 'text/plain'});
 
     download.setAttribute('href', window.URL.createObjectURL(bb));
     download.setAttribute('download', filename);
+    download.innerText = 'Click here to download';
 
-    download.dataset.downloadurl = ['text/plain', download.download, pom.href].join(':');
+    download.dataset.downloadurl = ['text/plain', download.download, download.href].join(':');
     download.draggable = true; 
     download.classList.add('dragout');
+
+    document.body.appendChild(download)
 };
-
-createButton.addEventListener('click', downloadHandler);
-
-/* var xmltext = "<sometag><someothertag></someothertag></sometag>";
-
-var filename = "file.xml";
-var pom = document.createElement('a');
-var bb = new Blob([xmltext], {type: 'text/plain'});
-
-pom.setAttribute('href', window.URL.createObjectURL(bb));
-pom.setAttribute('download', filename);
-
-pom.dataset.downloadurl = ['text/plain', pom.download, pom.href].join(':');
-pom.draggable = true; 
-pom.classList.add('dragout');
-
-pom.click(); */
+ 
+document.querySelector('#xml-form').addEventListener('submit', downloadHandler);
